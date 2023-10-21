f0e7c728b7028e7b0a8da2334d1eb94ae053df7c
daniel skapunk <daniel.skapunk@gmail.com>
2023-10-21 03:38:36 -0600

    collection working

---
+++ src/content/config.ts
@@ -0,0 +1,25 @@
+// 1. Import utilities from `astro:content`
+
+import { defineCollection, z } from "astro:content";
+
+// 2. Define your collection(s)
+const acroCollection = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      aka: z.array(z.string()),
+      level: z.enum(["easy", "medium", "hard"]),
+      image: image().refine((img) => img.width >= 600, {
+        message: "Cover image must be at least 1080 pixels wide!",
+      }),
+      tags: z.array(z.string()),
+      numPeople: z.enum(["two", "three", "more"]),
+    }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  acro: acroCollection,
+};
