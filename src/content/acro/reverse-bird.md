c2b517057be17312a60c6cfd253b9850e3f7e0fe
daniel skapunk <daniel.skapunk@gmail.com>
2023-11-29 20:56:02 -0600

    info section into component, slug added to transition

---
+++ src/content/acro/reverse-bird.md
@@ -0,0 +1,33 @@
+---
+# layout: ../../layouts/Trick.astro
+name: Reverse Bird
+aka: [palomita reversa]
+level: easy
+image: './images/acroyoga-reverse-bird.jpeg'
+tags: []
+to:
+  - pose: Foot to Hand
+    slug: foot-to-hand
+    video: ''
+    canGoBack: true
+numPeople: two
+---
+
+## Drills
+
+[no video](https://www.youtube.com/)
+
+## Variantes
+
+- Palomita apoyando las dos manos
+- Palomita una mano
+- Palomita sin manos
+- Palomita un pie una mano
+  </br>
+
+:::section{.variantes}
+
+- [![one foot](./images/bird-variation-one-foot-and-shin.jpg)](https://www.instagram.com/p/BunhHsqFBUI/?utm_source=ig_web_button_share_sheet)
+  _one foot, one foot on shin_
+
+:::
--- src/content/config.ts
+++ src/content/config.ts
@@ -17,6 +17,7 @@ const acroCollection = defineCollection({
 			to: z.array(
 				z.object({
 					pose: z.string(),
+					slug: z.string(),
 					video: z.string(),
 					canGoBack: z.boolean()
 				})
@@ -26,8 +27,32 @@ const acroCollection = defineCollection({
 		})
 })
 
+const seqCollection = defineCollection({
+	type: 'content', // v2.5.0 and later
+	schema: ({ image }) =>
+		z.object({
+			name: z.string(),
+			aka: z.array(z.string()),
+			level: z.enum(['easy', 'medium', 'hard']),
+			image: image().refine((img) => img.width >= 600, {
+				message: 'Cover image must be at least 1080 pixels wide!'
+			}),
+			video: z.optional(z.string()),
+			// to: z.array(
+			// 	z.object({
+			// 		pose: z.string(),
+			// 		video: z.string(),
+			// 		canGoBack: z.boolean()
+			// 	})
+			// ),
+			tags: z.array(z.string()),
+			numPeople: z.enum(['two', 'three', 'more'])
+		})
+})
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-	acro: acroCollection
+	acro: acroCollection,
+	seq: seqCollection
 }
